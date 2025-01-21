2b31219bf4c76c1c7cb22823b0c0560823761707
gparry333 <gillian.parry@bjss.com>
2025-01-21 16:27:36 +0000

    CCM-8084: add template management e2e tests
    CCM-8084: remove amplify_outputs.json file
    
    CCM-8084: login fixture added
    
    CCM-8084: correct README
    
    CCM-8084: remove password

---
+++ tests/test-team/config/dev.config.ts
@@ -0,0 +1,29 @@
+import { defineConfig, devices } from '@playwright/test';
+import baseConfig from './playwright.config';
+
+export default defineConfig({
+  ...baseConfig,
+
+  timeout: 10_000,
+
+  expect: {
+    timeout: 10_000,
+  },
+
+  projects: [
+    {
+      name: 'e2e-dev',
+      testMatch: '*-e2e.ts',
+      use: {
+        screenshot: 'only-on-failure',
+        baseURL: 'https://main.web-gateway.dev.nhsnotify.national.nhs.uk',
+        ...devices['Desktop Chrome'],
+        headless: false,
+        storageState: 'auth.json', // Load saved authentication state
+        launchOptions: {
+          slowMo: 100,
+        },
+      },
+    },
+  ],
+});
---
+++ tests/test-team/functions/template-mgmt-e2e-common-steps.ts
@@ -0,0 +1,122 @@
+import { loggedInTest as test, expect } from '../fixtures/login';
+import { TemplateMgmtBasePage } from '../pages/template-mgmt-base-page';
+
+type CommonStepsProps = {
+  basePage: TemplateMgmtBasePage;
+  baseURL?: string;
+};
+
+export function startPage({ basePage, baseURL }: CommonStepsProps) {
+  return test.step('start page', async () => {
+    await basePage.navigateTo(
+      `${baseURL}/templates/create-and-submit-templates`
+    );
+    await expect(basePage.page).toHaveURL(
+      `${baseURL}/templates/create-and-submit-templates`
+    );
+    await expect(basePage.pageHeader).toHaveText(
+      'Create and submit a template to NHS Notify'
+    );
+    await basePage.clickButtonByName('Start now');
+  });
+}
+
+export function startNewTemplate({ basePage }: CommonStepsProps) {
+  return test.step('start template process', async () => {
+    await basePage.clickButtonByName('Create template');
+  });
+}
+
+export function chooseTemplate(
+  { basePage, baseURL }: CommonStepsProps,
+  channel: string
+) {
+  return test.step('Choose template type', async () => {
+    await expect(basePage.page).toHaveURL(
+      `${baseURL}/templates/choose-a-template-type`
+    );
+
+    await expect(basePage.pageHeader).toHaveText(
+      'Choose a template type to create'
+    );
+
+    await basePage.checkRadio(channel);
+
+    await basePage.clickButtonByName('Continue');
+  });
+}
+
+export function createTemplate(
+  { basePage, baseURL }: CommonStepsProps,
+  channel: string,
+  channelPath: string
+) {
+  return test.step('Create template', async () => {
+    await expect(basePage.page).toHaveURL(
+      `${baseURL}/templates/create-${channelPath}-template`
+    );
+    if (channel === 'Email') {
+      await expect(basePage.pageHeader).toHaveText(`Create email template`);
+    } else if (channel === 'Text message (SMS)') {
+      await expect(basePage.pageHeader).toHaveText(
+        `Create text message template`
+      );
+    } else {
+      await expect(basePage.pageHeader).toHaveText(
+        `Create ${channel} template`
+      );
+    }
+
+    await basePage.fillTextBox('Template name', 'E2E Name');
+
+    if (channel === 'Email') {
+      await basePage.fillTextBox('Subject line', 'E2E subject');
+    }
+
+    await basePage.fillTextBox('Message', 'E2E Message');
+
+    await basePage.clickButtonByName('Save and preview');
+  });
+}
+
+export function previewPage(
+  { basePage, baseURL }: CommonStepsProps,
+  channelPath: string
+) {
+  return test.step('Preview page', async () => {
+    await expect(basePage.page).toHaveURL(
+      // eslint-disable-next-line security/detect-non-literal-regexp
+      new RegExp(`${baseURL}/templates/preview-${channelPath}-template/(.*)`)
+    );
+
+    await expect(basePage.pageHeader).toHaveText('E2E Name');
+
+    await basePage.checkRadio('Submit template');
+
+    await basePage.clickButtonByName('Continue');
+  });
+}
+
+export function submitPage(
+  { basePage, baseURL }: CommonStepsProps,
+  channelPath: string
+) {
+  return test.step('Submit page', async () => {
+    await expect(basePage.page).toHaveURL(
+      // eslint-disable-next-line security/detect-non-literal-regexp
+      new RegExp(`${baseURL}/templates/submit-${channelPath}-template/(.*)`)
+    );
+
+    await expect(basePage.pageHeader).toHaveText(`Submit 'E2E Name'`);
+
+    await basePage.clickButtonByName('Submit template');
+
+    // Submitted Page
+    await expect(basePage.page).toHaveURL(
+      // eslint-disable-next-line security/detect-non-literal-regexp
+      new RegExp(`${baseURL}/templates/${channelPath}-template-submitted/(.*)`)
+    );
+
+    await expect(basePage.pageHeader).toHaveText('Template submitted');
+  });
+}
