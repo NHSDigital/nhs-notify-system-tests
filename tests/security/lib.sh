f84076b5bab7f703feb8d30ad28cf15b6ee3f9c3
Alex Nuttall <alex.nuttall1@nhs.net>
2025-02-05 10:23:18 +0000

    CCM-5340: add rule configuration

--- tests/security/lib.sh
+++ tests/security/lib.sh
@@ -29,7 +29,7 @@ get_branch_segment() {
   local template_management_branch="$1"
 
   if [ -n "$template_management_branch" ]; then
-    template_management_branch="~$(echo $template_management_branch | tr -d '/')"
+    echo "~$(echo $template_management_branch | tr -d '/_' | tr [:upper:] '[:lower:]')"
   fi
 }
 
