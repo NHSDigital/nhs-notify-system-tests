105a620bbebf173d77590923280056a5e35447d0
Mark Ramsden <mark.ramsden5@nhs.net>
2025-02-12 07:00:39 +0000

    CCM-7849: file formatting

--- tests/test-team/config/dev.config.ts
+++ tests/test-team/config/dev.config.ts
@@ -26,7 +26,7 @@ export default defineConfig({
         headless: false,
         storageState: { cookies: [], origins: [] },
         launchOptions: {
-          slowMo: 100,
+          slowMo: 1100,
         },
         video: 'on'
       },
