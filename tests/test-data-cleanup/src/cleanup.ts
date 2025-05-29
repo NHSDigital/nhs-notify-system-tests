8f9cc4bc4291c5a140b0802623fc36a584908be9
Mark Ramsden <mark.ramsden5@nhs.net>
2025-05-29 14:12:24 +0100

    CCM-10132: tidy

--- tests/test-data-cleanup/src/cleanup.ts
+++ tests/test-data-cleanup/src/cleanup.ts
@@ -4,16 +4,15 @@ import {
   DynamoDBClient,
   QueryCommand,
   QueryCommandInput,
-  TransactWriteItemsCommand,
-} from "@aws-sdk/client-dynamodb";
+} from '@aws-sdk/client-dynamodb';
 
-const client = new DynamoDBClient({ region: "eu-west-2" });
+const client = new DynamoDBClient({ region: 'eu-west-2' });
 
-const testUsers = JSON.parse(process.env.TEST_USERS ?? "[]") as Array<string>;
+const testUsers = JSON.parse(process.env.TEST_USERS ?? '[]') as Array<string>;
 const targetEnvironment = process.env.TARGET_ENVIRONMENT;
 
 if (!targetEnvironment) {
-  throw new Error("Missing TARGET_ENVIRONMENT");
+  throw new Error('Missing TARGET_ENVIRONMENT');
 }
 
 console.log(`Test users (${testUsers.length}): ${JSON.stringify(testUsers)}`);
@@ -23,12 +22,12 @@ const tableName = `nhs-notify-${targetEnvironment}-app-api-templates`;
 async function deleteTestUserData(owner: string): Promise<void> {
   const input: QueryCommandInput = {
     TableName: tableName,
-    KeyConditionExpression: "#owner = :owner",
+    KeyConditionExpression: '#owner = :owner',
     ExpressionAttributeNames: {
-      "#owner": "owner",
+      '#owner': 'owner',
     },
     ExpressionAttributeValues: {
-      ":owner": { S: owner },
+      ':owner': { S: owner },
     },
   };
 
@@ -47,17 +46,19 @@ async function deleteTestUserData(owner: string): Promise<void> {
 
   console.log(`Found ${items.length} items for owner ${owner}`);
 
-  const itemKeys = items.map(item => ({
+  const itemKeys = items.map((item) => ({
     owner: item.owner,
-    id: item.id
+    id: item.id,
   }));
 
   for (let itemKey of itemKeys) {
     console.log(`Deleting ${JSON.stringify(itemKey)}`);
-    await client.send(new DeleteItemCommand({
-      TableName: tableName,
-      Key: itemKey
-    }));
+    await client.send(
+      new DeleteItemCommand({
+        TableName: tableName,
+        Key: itemKey,
+      })
+    );
   }
 }
 
