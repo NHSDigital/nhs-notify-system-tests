1449e65ccb4da02dc8dd0fdddea78915292f2f73
Michael Harrison <michael.harrison31@nhs.net>
2025-06-26 10:14:19 +0100

    CCM-9750: bash script fix

--- tests/test-team/lifecycle/templates/setup.sh
+++ tests/test-team/lifecycle/templates/setup.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-local TARGET_ENVIRONMENT=$1
+TARGET_ENVIRONMENT=$1
 
 echo "Hello from the product test templates setup script."
 echo "Target Environment $TARGET_ENVIRONMENT"
--- tests/test-team/lifecycle/templates/teardown.sh
+++ tests/test-team/lifecycle/templates/teardown.sh
@@ -2,7 +2,7 @@
 
 set -euo pipefail
 
-local TARGET_ENVIRONMENT=$1
+TARGET_ENVIRONMENT=$1
 
 echo "Hello from the product test templates teardown script."
 echo "Target Environment $TARGET_ENVIRONMENT"
