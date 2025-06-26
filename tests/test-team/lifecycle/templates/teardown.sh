049026d18572a0d00739187670299210f7e55c1b
Michael Harrison <michael.harrison31@nhs.net>
2025-06-26 11:51:49 +0100

    CCM-9750: add mechanism for persisting state between setup and teardown

--- tests/test-team/lifecycle/templates/teardown.sh
+++ tests/test-team/lifecycle/templates/teardown.sh
@@ -2,6 +2,8 @@
 
 set -euo pipefail
 
+script_path="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )";
+
 TARGET_ENVIRONMENT=$1
 
 echo "Running the product test templates teardown script..."
@@ -9,5 +11,7 @@ echo "Target Environment: $TARGET_ENVIRONMENT"
 
 sftp_poll_rule_name="nhs-notify-$TARGET_ENVIRONMENT-app-api-sftp-poll-wtmmock"
 
-aws events put-rule --name $sftp_poll_rule_name --schedule-expression "rate(1 hour)"
+sftp_poll_rule_initial_rate=$(cat $script_path/setup.json | jq -r .initialState.rules.[\"$sftp_poll_rule_name\"].ScheduleExpression)
+
+aws events put-rule --name $sftp_poll_rule_name --schedule-expression "$sftp_poll_rule_initial_rate"
 
