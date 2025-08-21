3ca38446c58260650d12f598fb86df5125eb3f63
gparry333 <gillian.parry@bjss.com>
2025-08-21 15:14:31 +0100

    CCM-10941: fix for failing copy test in Product tests

--- tests/test-team/pages/template-mgmt-base-page.ts
+++ tests/test-team/pages/template-mgmt-base-page.ts
@@ -25,7 +25,7 @@ export class TemplateMgmtBasePage {
 
   readonly templateToDelete: Locator;
 
-  readonly templateEdited: Locator;
+  readonly templateEdited: (templateName: string) => Locator;
 
   constructor(page: Page) {
     this.page = page;
@@ -56,7 +56,8 @@ export class TemplateMgmtBasePage {
 
     this.templateToDelete = page.getByRole('link', { name: 'Test delete', exact: true });
 
-    this.templateEdited = page.getByRole('link', { name: 'Test edit changed', exact: true })
+    this.templateEdited = (templateName: string) =>
+      page.getByRole('link', { name: templateName, exact: true });
 
     this.skipLink = page
       .locator('[id="skip-link"]')
