22dfe53aa2fe62bca6862de9acfb51aaf501aa53
gparry333 <gillian.parry@cgi.com>
2025-10-01 09:50:15 +0100

    CCM-12435: fix to Playwright tests (#110)

--- tests/security/tests/template-mgmt-cis2-login.security.ts
+++ tests/security/tests/template-mgmt-cis2-login.security.ts
@@ -10,6 +10,7 @@ import {
   startPage,
 } from '../functions/common-steps';
 import test from 'playwright/test';
+import { TemplateMgmtLetterPage } from '../pages/template-mgmt-letter-page';
 
 test.use({ storageState: { cookies: [], origins: [] } });
 
@@ -33,15 +34,17 @@ test('User logs in via CIS2, saves data in templates, logs out and logs back in 
   }
 
   const basePage = new TemplateMgmtBasePage(page);
+  const letterPage = new TemplateMgmtLetterPage(page);
   const props = {
     basePage,
     baseURL,
+    letterPage
   };
   const channel = 'Email';
   const channelPath = 'email';
   const name = 'E2E Name';
 
-  await startPage({ basePage, baseURL });
+  await startPage({ basePage, baseURL, letterPage });
   await loginWithCis2(basePage, 'Message templates');
   await startNewTemplate(props);
   await chooseTemplate(props, channel);
@@ -50,6 +53,6 @@ test('User logs in via CIS2, saves data in templates, logs out and logs back in 
   await context.storageState({ path: 'cis2.json' });
   await logOut(basePage);
   await page.waitForLoadState('networkidle');
-  await startPage({ basePage, baseURL });
+  await startPage({ basePage, baseURL, letterPage });
   await loginWithCis2(basePage, 'Message templates');
 });
